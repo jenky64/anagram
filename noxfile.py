90681ed30d6b6b8c664603d26a0b284392ea3a17
David Jenkins <jenky64@gmail.com>
2021-06-01 02:49:49 -0600

    install packages from package-list.txt

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@ import nox
 
 @nox.session(python=["3.8"],venv_backend="conda")
 def tests(session):
-    session.conda_install('pytest','peewee','pytest-order')
+    session.conda_install('--file','package-list.txt')
     session.run('conda', 
             'develop', 
             '.',
