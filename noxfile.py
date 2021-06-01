d71d52285ea6abf71ca9edda45027da42b7a086c
David Jenkins <jenky64@gmail.com>
2021-06-01 02:44:58 -0600

    added noxfile.py

---
+++ noxfile.py
@@ -0,0 +1,11 @@
+import nox
+
+@nox.session(python=["3.8"],venv_backend="conda")
+def tests(session):
+    session.conda_install('pytest','peewee','pytest-order')
+    session.run('conda', 
+            'develop', 
+            '.',
+            '--prefix', 
+            session.virtualenv.location)
+    session.run('pytest')
