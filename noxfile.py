2c4ac52188e31dcc186a85e48f8d56c700cc655c
davidj <jenky64@gmail.com>
2021-06-12 22:26:26 -0600

    added session name to output html file

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
 import nox
 
-@nox.session(python=["3.7","3.8"],venv_backend="conda")
+@nox.session(python=["3.7","3.8"],venv_backend="conda",reuse_venv=True)
 def tests(session):
     session.conda_install('--file','package-list.txt')
     session.run('conda', 
@@ -8,7 +8,7 @@ def tests(session):
             '.',
             '--prefix', 
             session.virtualenv.location)
-    session.run('pytest', '-v', '--html=test_results.html')
+    session.run('pytest', '-v', f'--html=test_results-{session.name}.html')
 
 @nox.session(python=["3.8"],venv_backend="conda")
 def lint(session):
