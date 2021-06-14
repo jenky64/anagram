d0f382298ce391caa8da373a8b6854ffa06125f8
davidj <jenky64@gmail.com>
2021-06-14 13:03:21 -0600

    reverted comments

--- runtests.sh
+++ runtests.sh
@@ -2,5 +2,6 @@
 
 conda develop /app
 nox -R -s tests
-rm -rf /app/package-list.txt /app/noxfile.py
+rm -rf /app/module-list.txt /app/noxfile.py
 rm -rf /app/tests /app/anagram /app/assets
+tail -f /dev/null
