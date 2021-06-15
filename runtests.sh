ff327ae3a618a91664aaa41a151e4488eb6d1611
davidj <jenky64@gmail.com>
2021-06-14 23:31:10 -0600

    added switch to reuse env

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
+REUSE=False
+
+while getopts "r" opt; do
+    case "${opt}" in
+      r) REUSE=True
+         ;;
+      *) echo "invalid option"
+         exit 1
+         ;;
+    esac
+done
+
+
 conda develop /app
-nox -R -s tests
-rm -rf /app/module-list.txt /app/noxfile.py
+
+# check if we can reuse the nox environments or not
+if [[ "${REUSE}" = "True" ]]; then
+    nox -R -s tests
+else
+    nox -s tests
+fi
+
+# make backup copies of module-list.txt and noxfile.py
+mv /app/modules-list.txt /app/modules-list.prev
+mv /app/testing-modules-list.txt /app/testing-modules-list.prev
+mv /app/noxfile.py /app/noxfile.prev
+
+# remove code directories
 rm -rf /app/tests /app/anagram /app/assets
+
 tail -f /dev/null
