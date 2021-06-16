dc214c0babf2ee05cb98db963fbac4c750e61f58
davidj <jenky64@gmail.com>
2021-06-16 15:55:05 -0600

    significant changes

--- runtests.sh
+++ runtests.sh
@@ -1,26 +1,76 @@
 #!/bin/bash
 
-REUSE=False
+REUSE=0
+FAIL=0
+CMP=0
+CHANGED=0
 
-while getopts "r" opt; do
-    case "${opt}" in
-      r) REUSE=True
-         ;;
-      *) echo "invalid option"
-         exit 1
-         ;;
-    esac
+# list of files to check
+declare -a files=("noxfile.py" "modules-list.txt" "testing-modules-list.txt")
+
+# compare current and previous file
+# if the are different, set REUSE to False
+compare_files() {
+    cmp --silent $1 $2
+
+    CMP=$?
+    if [[ ${REUSE} -eq 0 ]]; then
+        REUSE=$?
+    fi
+}
+
+# set FAIL=1 if any of the config files are missing
+for fname in "${files[@]}"; do
+    if [[ ! -f ${fname} ]]; then
+        echo "file ${fname} is missing." >> runtests.log
+        FAIL=1
+    fi
 done
 
+if [[ ${FAIL} -eq 1 ]]; then
+    echo "" >> runtests.log
+    echo "**************************************" >> runtests.log
+    echo "cannot run tests due to missing files." >> runtests.log
+    echo "**************************************" >> runtests.log
+    echo "" >> runtests.log
+fi
+
+for fname in "${files[@]}"; do
+    if [[ ! -f ${fname%.*}.prev ]]; then
+        echo "file ${fname%.*}.prev is missing." >>runtests.log
+        REUSE=1
+        continue
+    fi
+
+    compare_files ${fname} ${fname%.*}.prev
+
+    if [[ "${CMP}" -eq 1 ]]; then
+        CHANGED=1
+        echo "file comparison determined ${fname} has changed" >> runtests.log
+    fi
+done
+
+if [[ ${REUSE} -eq 1 || ${CHANGED} -eq 1 ]]; then
+    echo "" >> runtests.log
+    echo "" >> runtests.log
+    echo "*********************************************************" >> runtests.log
+    echo "cannot reuse environemts due to missing or changed files." >> runtests.log
+    echo "*********************************************************" >> runtests.log
+    echo "" >> runtests.log
+fi
+
+if [[ ${FAIL} -eq 1 ]]; then
+    exit 1
+fi
 
 conda develop /app
 
-# check if we can reuse the nox environments or not
-if [[ "${REUSE}" = "True" ]]; then
+if [[ "${REUSE}" -eq 0 ]]; then
     nox -R -s tests
 else
     nox -s tests
 fi
+
 
 # make backup copies of module-list.txt and noxfile.py
 mv /app/modules-list.txt /app/modules-list.prev
