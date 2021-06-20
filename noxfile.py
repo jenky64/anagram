7ab8a688b5acee9fff5a12e38a78dc6d35f3cd54
davidj <jenky64@gmail.com>
2021-06-20 17:25:56 -0600

    u

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,9 @@
 import nox
 import pathlib
 
+results: list = []
 #@nox.session(python=["3.7","3.8"],venv_backend="conda")
-@nox.session(python=["3.8"],venv_backend="conda")
+@nox.session(python=["3.7", "3.8"],venv_backend="conda")
 def tests(session):
     session.conda_install('--channel=conda-forge', '--file', 'testing-modules-list.txt')
     session.conda_install('--channel=conda-forge', '--file', 'modules-list.txt')
@@ -11,10 +12,8 @@ def tests(session):
             '.',
             '--prefix', 
             session.virtualenv.location)
-    try:
-        session.run('pytest', '-v', f'--html=test_results-{session.name}.html')
-    except:
-        print(f'fail')
+
+    session.run('pytest', '-vv', f'--html=test_results-{session.name}.html')
 
 @nox.session(python=["3.8"],venv_backend="conda")
 def lint(session):
